39e65b28e08206f82a12ca5d40424b522efaf4bb
H.Emin Yıldız <46842174+Heminyildiz@users.noreply.github.com>
2025-02-18 11:59:48 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        lexend: ["Lexend", "sans-serif"],
+      },
       colors: {
-        // Pastel renkler
-        "pastel-100": "#E3F2FD",
-        "pastel-200": "#FFF9C4",
-        "pastel-300": "#FFE0B2",
-        "pastel-400": "#FFCDD2",
+        "pastel-100": "#E3F2FD", 
+        "pastel-200": "#FFF9C4", 
+        "pastel-300": "#FFE0B2", 
+        "pastel-400": "#FFCDD2", 
         "pastel-500": "#D1C4E9",
       }
     },
   },
   plugins: [],
 };
+
 
