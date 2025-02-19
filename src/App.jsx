34f7d0dd716e83b848b3d07f6dd23d7ad93204d4
H.Emin Yıldız <46842174+Heminyildiz@users.noreply.github.com>
2025-02-19 16:47:00 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import MainGame from "./pages/MainGame";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      {/* Tek sayfalık oyunumuz */}
-      <MainGame />
-    </div>
+    <Routes>
+      {/* Ana oyunumuz */}
+      <Route path="/" element={<MainGame />} />
+
+      {/* Footer linkleri */}
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      <Route path="/contact" element={<Contact />} />
+
+      {/* Catch-all => yönlendirme */}
+      <Route path="*" element={<MainGame />} />
+    </Routes>
   );
 }
 
 export default App;
+
 
 
 
